24f51bddf00f38f9103dd4ae6b89e7a200b3728d
Marnen Laibow-Koser <marnen@marnen.org>
2013-06-02 12:55:09 -0400

    Refactor.

--- features/step_definitions/utility_steps.rb
+++ features/step_definitions/utility_steps.rb
@@ -2,18 +2,16 @@ When /^I go to the root in edit mode$/ do
   visit '/?mode=edit'
 end
 
-Then /^I should see the HTML "(.*?)" within the content$/ do |html|
-  within '#block_content .content' do
-    page.html.should include html
-  end
-end
-
-Then /^I should not see "(.*?)" within the content$/ do |text|
-  within '#block_content .content' do
-    page.should_not have_content text
-  end
+Then /^I should see the HTML "(.*?)"$/ do |html|
+  page.html.should include html
 end
 
 Then /^I should not see "(.*?)"$/ do |text|
   page.should_not have_content text
+end
+
+Then /^(.*?) within the content/ do |step_text|
+  within '#block_content .content' do
+    step step_text
+  end
 end
