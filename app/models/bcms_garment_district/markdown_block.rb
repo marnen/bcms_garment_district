44a2431f76df179db72ff7d3d0c069268210d438
Marnen Laibow-Koser <marnen@marnen.org>
2013-06-02 12:46:57 -0400

    Render Markdown in model.

--- app/models/bcms_garment_district/markdown_block.rb
+++ app/models/bcms_garment_district/markdown_block.rb
@@ -1,6 +1,12 @@
+require 'maruku'
+
 module BcmsGarmentDistrict
   class MarkdownBlock < ActiveRecord::Base
     acts_as_content_block
     validates_presence_of :name, :content
+
+    def to_html
+      Maruku.new(content).to_html
+    end
   end
 end
--- spec/models/bcms_garment_district/markdown_block_spec.rb
+++ spec/models/bcms_garment_district/markdown_block_spec.rb
@@ -2,13 +2,21 @@ require 'spec_helper'
 
 module BcmsGarmentDistrict
   describe MarkdownBlock do
-    context 'general properties' do
+    describe 'general properties' do
       it 'should be a type of content block' do
         MarkdownBlock.new.should be_a_kind_of Cms::Acts::ContentBlock::MacroMethods::InstanceMethods
       end
 
       it { should validate_presence_of :name }
       it { should validate_presence_of :content }
+    end
+
+    describe '#to_html' do
+      it 'should return the content rendered into HTML' do
+        content = 'Here is some *Markdown*.'
+        block = FactoryGirl.create :markdown_block, :content => content
+        block.to_html.should == Maruku.new(content).to_html
+      end
     end
   end
 end
