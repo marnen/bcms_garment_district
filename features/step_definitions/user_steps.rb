68bc7d6676ccb78fcadece9ce19db2ac5f1e9000
Marnen Laibow-Koser <marnen@marnen.org>
2013-06-02 12:52:32 -0400

    Remove old syntax.

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,6 +1,6 @@
 Given /^I am logged in as an administrator$/ do
   password = 'passw0rd'
-  user = Factory :admin_user, :password => password
+  user = FactoryGirl.create :admin_user, :password => password
   visit login_path
   fill_in 'login', :with => user.login
   fill_in 'password', :with => password
--- features/support/factory_girl.rb
+++ features/support/factory_girl.rb
@@ -2,11 +2,3 @@ require 'factory_girl'
 require 'ffaker'
 
 FactoryGirl.find_definitions
-
-module FactoryGirl::Shorthand
-  def Factory(*args)
-    FactoryGirl.create *args
-  end
-end
-
-World FactoryGirl::Shorthand
